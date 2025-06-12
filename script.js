f9acb78f90690dc89f3833dcc7e61be055f360ee
qusaihm <qusaiaboaabedm4@gmail.com>
2025-06-13 01:23:15 +0300

    function for validate

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 const addTaskButtton = document.getElementById("add-task");
 const taskList = document.getElementById("task-list");
 const noTaskMsg = document.getElementById("masseg-notask");
+ 
+const isValidTask = (text) => {
+  if (text.trim() === '') {
+    inputError.textContent = 'Task cannot be empty';
+    inputError.style.display = 'block';
+    return false;
+  }
+
+  if (/^\d/.test(text)) {
+    inputError.textContent = 'Task cannot wtart with a number';
+    inputError.style.display = 'block';
+    return false;
+  }
+
+  if (text.trim().length < 5) {
+    inputError.textContent = 'Task must be at least 5 characters long';
+    inputError.style.display = 'block';
+    return false;
+  }
+
+  inputError.style.display = 'none';  
+  return true;
+};
